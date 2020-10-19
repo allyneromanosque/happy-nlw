d2418cfad3b98dac8416f14bf29ce3f491639d0e
Romanosque <allyneromanosque@gmail.com>
2020-10-19 10:26:54 -0400

    creditos maps

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -15,7 +15,10 @@ const lng = document.querySelector("span[data-lng]").dataset.lng;
 const map = L.map("mapid", options).setView([lat, lng], 15);
 
 //create and add tileLayer
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
 
 //create icon
 const icon = L.icon({
